b8f6ea61354f6cfe10a360efeda52f4c73ef4cfc
wangbing <wangbing1985@163.com>
2016-05-10 16:29:55 +0800

    add default work bat

---
+++ default_rsync_server_install_online.bat
@@ -0,0 +1,10 @@
+@echo off
+set usr=rsyncuser
+set pwd=rsyncpwd
+call rsync_server_cygwin\download.bat
+call rsync_server_cygwin\install_download_form_constant_to_local.bat
+call rsync_server_cygwin\install_from_local.bat
+call windows2012r2\create_admin.bat "%usr%" "%pwd%"
+call windows2012r2\granting_user_rights_to_login_as_service.bat "%usr%"
+call rsync_server_cygwin\install_rsync_deamon_as_service.bat "%usr%" "%pwd%"
+call rsync_server_cygwin\cwrsync_test_self.bat
