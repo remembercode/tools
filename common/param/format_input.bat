c62533a0288de64bebbcbd200be782af5a84db27
wangbing <wangbing1985@163.com>
2016-05-11 15:40:10 +0800

    fix input is null but output "="

--- common/param/format_input.bat
+++ common/param/format_input.bat
@@ -3,6 +3,10 @@ REM receive one param, delete all " ' ? :
 :receive_param
 setlocal enabledelayedexpansion
 set input=%~1
+if "!input!" EQU "" (
+	echo ""
+	goto :eof
+)
 @rem remove double quotes
 set input=!input:"=!
 @rem remove single quotes
