8c8e08a175373b7d715fab4eef6a6d2cc13a61d8
wangbing <wangbing1985@163.com>
2016-05-12 09:53:36 +0800

    fix test.bat to call sql2008r2\backup_sql_with_fulltime_folder.bat

--- test.bat
+++ test.bat
@@ -8,5 +8,5 @@ REM call windows2012r2\create_admin.bat "%usr%" "%pwd%"
 REM call windows2012r2\granting_user_rights_to_login_as_service.bat "%usr%"
 REM call rsync_server_cygwin\install_rsync_deamon_as_service.bat "%usr%" "%pwd%"
 REM call rsync_server_cygwin\cwrsync_test_self.bat
-call sql2008r2\backup_sql.bat "MSSQLSERVER" "Test" "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA" "D:\SQL_Server_2008_R2_Backup\"
+call sql2008r2\backup_sql_with_fulltime_folder.bat "MSSQLSERVER" "Test" "C:\Program Files\Microsoft SQL Server\MSSQL11.MSSQLSERVER\MSSQL\DATA" "D:\SQL_Server_2008_R2_Backup\"
 REM "MSSQLSERVER"
